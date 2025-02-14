d02a413c744a1a23225565754c81172adc6ec3d7
Gezim-gif <kokoshi24.gk@gmail.com>
2025-02-14 16:50:46 +0100

    up

--- scripts.js
+++ scripts.js
@@ -46,3 +46,98 @@ work3.onclick = function() {
     }
 }
 
+var modal4 = document.getElementById("myModal4");
+var work4 = document.getElementById("work4");
+var span4 = document.getElementsByClassName("close4")[0];
+
+work4.onclick = function() {
+    modal4.style.display = "block";
+  }
+  span4.onclick = function() {
+    modal4.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal4.style.display = "none";
+    }
+}
+
+var modal5 = document.getElementById("myModal5");
+var work5 = document.getElementById("work5");
+var span5 = document.getElementsByClassName("close5")[0];
+
+work5.onclick = function() {
+    modal5.style.display = "block";
+  }
+  span5.onclick = function() {
+    modal5.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal5.style.display = "none";
+    }
+}
+
+var modal6 = document.getElementById("myModal6");
+var work6 = document.getElementById("work6");
+var span6 = document.getElementsByClassName("close6")[0];
+
+work6.onclick = function() {
+    modal6.style.display = "block";
+  }
+  span6.onclick = function() {
+    modal6.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal6.style.display = "none";
+    }
+}
+
+var modal7 = document.getElementById("myModal7");
+var work7 = document.getElementById("work7");
+var span7 = document.getElementsByClassName("close7")[0];
+
+work7.onclick = function() {
+    modal7.style.display = "block";
+  }
+  span7.onclick = function() {
+    modal7.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal7.style.display = "none";
+    }
+}
+
+var modal8 = document.getElementById("myModal8");
+var work8 = document.getElementById("work8");
+var span8 = document.getElementsByClassName("close8")[0];
+
+work8.onclick = function() {
+    modal8.style.display = "block";
+  }
+  span8.onclick = function() {
+    modal8.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal8.style.display = "none";
+    }
+}
+
+var modal9 = document.getElementById("myModal9");
+var work9 = document.getElementById("work9");
+var span9 = document.getElementsByClassName("close9")[0];
+
+work9.onclick = function() {
+    modal9.style.display = "block";
+  }
+  span9.onclick = function() {
+    modal9.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal9.style.display = "none";
+    }
+}
